4b32f468e8ffb3b64511ee2e9601deb43c02a31a
Carlos Azevedo <cguerraazevedo@tecnico.ulisboa.pt>
2018-09-21 11:48:29 +0100

    updated create ros package script

--- resources/scripts/create_ros_pkg.sh
+++ resources/scripts/create_ros_pkg.sh
@@ -9,22 +9,23 @@ fi
 
 # assign first received argument to variable PKG_NAME
 PKG_NAME=${1}
-AUTHOR_NAME='Autor Name'
-AUTHOR_EMAIL='autor.email@isr.tecnico.ulisboa.pt'
-MAINTAINER_NAME='Maintainer Name'
-MAINTAINER_EMAIL='maintainer.name@isr.tecnico.ulisboa.pt'
+AUTHOR_NAME="Author Name"
+AUTHOR_EMAIL="author.email@tecnico.ulisboa.pt"
+MAINTAINER_NAME="Maintainer Name"
+MAINTAINER_EMAIL="maintainer.name@tecnico.ulisboa.pt"
 
 # setup file content
 CMAKELISTS_CONTENT="cmake_minimum_required(VERSION 2.8.3)\nproject(${PKG_NAME})\n\nfind_package(catkin REQUIRED\n  COMPONENTS\n)\n\ncatkin_python_setup()\n\ncatkin_package(\n    CATKIN_DEPENDS\n)"
-ROS_INDEPENDAT_CLASS_CONTENT="#!/usr/bin/env python\n\ndef my_generic_sum_function(a, b):\n    \"\"\"\n    TODO!! documentation\n    \"\"\"\n    return a + b"
-PACKAGE_XML_CONTENT="<?xml version=\"1.0\"?>\n<package>\n  <name>${PKG_NAME}</name>\n  <version>1.0.0</version>\n  <description>\n    TODO!\n  </description>\n\n  <license>GPLv3</license>\n\n  <author email=\"olima_84@yahoo.com\">${AUTHOR_NAME}</author>\n  <maintainer email=\"${MAINTAINER_EMAIL}\">${MAINTAINER_NAME}</maintainer>\n\n  <buildtool_depend>catkin</buildtool_depend>\n\n</package>"
-CONFIG_YAML_CONTENT="#TODO!!"
+ROS_INDEPENDENT_CLASS_CONTENT="#!/usr/bin/env python\n\ndef my_generic_sum_function(a, b):\n    \"\"\"\n    TODO!! documentation\n    \"\"\"\n    return a + b"
+PACKAGE_XML_CONTENT="<?xml version=\"1.0\"?>\n<package>\n  <name>${PKG_NAME}</name>\n  <version>1.0.0</version>\n  <description>\n    TODO!\n  </description>\n\n  <license>GPLv3</license>\n\n  <author email=\"${AUTHOR_EMAIL}\">${AUTHOR_NAME}</author>\n  <maintainer email=\"${MAINTAINER_EMAIL}\">${MAINTAINER_NAME}</maintainer>\n\n  <buildtool_depend>catkin</buildtool_depend>\n\n  <build_depend>TODO</build_depend>\n\n  <run_depend>TODO</run_depend>\n\n</package>"
+CONFIG_YAML_CONTENT="# TODO!!"
 README_CONTENT="${PKG_NAME} documentation"
 LAUNCH_FILE_CONTENT="<?xml version=\"1.0\"?>\n<launch>\n\n    <!-- small description about your node -->\n    \n    <node   pkg=\"my_package_name\" type=\"my_node_name\" name=\"my_node_name\"\n            respawn=\"false\" output=\"screen\" args=\"\$(find ${PKG_NAME}_name)/ros/config/my_arg_file.yaml\"/>\n\n</launch>"
-SCRIPTS_CONTENT="#!/usr/bin/env python\n\nimport ${PKG_NAME}_ros.${PKG_NAME}\n\nif __name__ == '__main__':\n    ${PKG_NAME}_ros.${PKG_NAME}.main()"
+SCRIPTS_CONTENT="#!/usr/bin/env python\n\nimport ${PKG_NAME}_ros.${PKG_NAME}_node\n\nif __name__ == '__main__':\n    ${PKG_NAME}_ros.${PKG_NAME}_node.main()"
 MY_NODE_IMPLEMENTATION_CONTENT=""
 MY_TEST_CONTENT=""
 SETUP_PY_CONTENT="#!/usr/bin/env python\n\nfrom distutils.core import setup\nfrom catkin_pkg.python_setup import generate_distutils_setup\n\n# for your packages to be recognized by python\nd = generate_distutils_setup(\n  packages=['${PKG_NAME}', '${PKG_NAME}_ros'],\n  package_dir={'${PKG_NAME}': 'common/src/${PKG_NAME}', '${PKG_NAME}_ros': 'ros/src/${PKG_NAME}_ros'}\n)\n\nsetup(**d)"
+ROS_NODE_CONTENT="#!/usr/bin/env python\n\nfrom ${PKG_NAME}.my_ros_independent_class import my_generic_sum_function\n\ndef main():\n    print 'success'"
 
 # create folder structure
 mkdir ${PKG_NAME}
@@ -40,14 +41,14 @@ mkdir -p ros/test
 # create file structure
 touch CMakeLists.txt
 touch common/src/${PKG_NAME}/__init__.py
-touch common/src/${PKG_NAME}/my_ros_independant_class.py
+touch common/src/${PKG_NAME}/my_ros_independent_class.py
 touch package.xml
 touch ros/config/config_${PKG_NAME}.yaml
 touch ros/doc/README.md
 touch ros/launch/${PKG_NAME}.launch
 touch ros/scripts/${PKG_NAME}_node
 touch ros/src/${PKG_NAME}_ros/__init__.py
-touch ros/src/${PKG_NAME}_ros/${PKG_NAME}.py
+touch ros/src/${PKG_NAME}_ros/${PKG_NAME}_node.py
 touch ros/test/${PKG_NAME}_test.py
 touch setup.py
 
@@ -56,7 +57,7 @@ chmod +x ros/scripts/${PKG_NAME}_node
 
 # fill files with information from parameter section
 echo -e $CMAKELISTS_CONTENT  > CMakeLists.txt
-echo -e $ROS_INDEPENDAT_CLASS_CONTENT > common/src/${PKG_NAME}/my_ros_independant_class.py
+echo -e $ROS_INDEPENDENT_CLASS_CONTENT > common/src/${PKG_NAME}/my_ros_independent_class.py
 echo -e $PACKAGE_XML_CONTENT > package.xml
 echo -e $CONFIG_YAML_CONTENT > ros/config/config_${PKG_NAME}.yaml
 echo -e $README_CONTENT > ros/doc/README.md
@@ -65,3 +66,7 @@ echo -e $SCRIPTS_CONTENT > ros/scripts/${PKG_NAME}_node
 echo -e $MY_NODE_IMPLEMENTATION_CONTENT
 echo -e $MY_TEST_CONTENT > ros/test/${PKG_NAME}_test.py
 echo -e $SETUP_PY_CONTENT > setup.py
+echo -e $ROS_NODE_CONTENT > ros/src/${PKG_NAME}_ros/${PKG_NAME}_node.py
+
+catkin build --this
+source ~/.bashrc
